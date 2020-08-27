1b8276a4b5152cb77aafd7f1c531b3a935d40eb1
-l <ebacallo@odin.cs.csubak.edu>
2020-08-27 12:22:26 -0700

    my changes during lab class

--- lab1.cpp
+++ lab1.cpp
@@ -6,39 +6,62 @@
 #include <string.h>
 #include <iostream> 
 #include <cstring> 
-
-using std::string;
-using std::cout;
-using std::cin;
-using std::endl;
+#include <fstream> 
+using namespace std;
 
 bool word_checker(string word) {
     bool check = 0;
+
     for (unsigned int i = 0; i < word.length() - 1 ; i++) {
+        
         if(word[i] == word[i+1]) {
             check = 1;
             break;
         }
-
     }
-
      return check;
 }
 
 
 int main() {
-    string word;
-    bool loop = 0;
-    while (loop == 0) {
-        cout <<"Please enter a word with duplicate letters." << endl;
-        cin >> word;
-        loop = word_checker(word);
 
-        if (loop == 0) {
-            cout <<"That isn't a word with duplicate letters." << endl;
-         }
-    }
+    string word;
+    string test = "test";
+    ifstream testfile;
+    bool loop = 0;
+    bool testloop;
     
-    cout << "That word ( " << word << " ) has duplicate letters!" << endl;
-    return 0;
+    while (loop == 0) {
+        cout <<"Please enter a word. To check if it has double letters. Or enter 'test' to test operation." << endl;
+        cin >> word;
+        if (word == test){
+            testfile.open("test.txt");
+            while (!testfile.eof()){
+                testloop = 0;
+                getline(testfile,word);
+                testloop = word_checker(word);
+                if (testfile.eof()){
+                break;
+                } 
+                else if (testloop == 1){
+                    cout<<" That word (" << word << ") has double letters!" << endl;
+                }
+                else{
+                    cout <<" That word (" << word << ") does not have double letters." << endl;
+                }
+             }               
+            loop = 1;
+            return 0;
+        }
+        else{  
+            
+            loop = word_checker(word);
+            if (loop == 0) {
+            cout << word << " does not have double letters." << endl;
+            }   
+
+        }
+    }
+            cout << "That word ( " << word << " ) has double letters!" << endl;
+            return 0;
 }
